dabb1e5e9651cdad18759795078aef530bb0e8c9
osama-alek1 <osamaalak12+new@gmail.com>
2026-02-27 21:30:17 +0300

    fixed configration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Only add 'base' if deploying to a subdirectory, e.g., base: '/dashboard'
+  // base: '/',
   server: {
     port: 3000,
     proxy: {
